d50b7a0e6863f0a3e1acb01546df579d518244b6
Jan van Heusde <janvheusde@gmail.com>
2022-05-12 15:54:56 -0400

    added location cards

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root                   to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'dependence', to: 'pages#index'
+  get 'pigionier', to: 'pages#show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
